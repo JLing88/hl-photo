1da7f35f71f658cc3e727755c07a7116ada9f464
Jesse Ling <jesse.ryan.ling@gmail.com>
2019-02-06 08:20:59 -0700

    Changes navbar to flexbox

--- spec/features/user_visits_homepage_spec.rb
+++ spec/features/user_visits_homepage_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 describe 'As a visitor' do
   context 'they visit the homepage' do
-    it 'displays a nav bar with links for HOME, ABOUT, OUR WORK, CONTACT' do
+    it 'displays a nav bar with links for Home, About, Our Work, Contact' do
       visit '/'
 
-      within('#nav') do
+      within('.navbar') do
         expect(page).to have_link('Home')
         expect(page).to have_link('About')
         expect(page).to have_link('Our Work')
